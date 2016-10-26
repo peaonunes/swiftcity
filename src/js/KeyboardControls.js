dbf5a90079559f08f3b1199bd33cec67216ec984
Rafael Nunes G. da Silveira <rngs@cin.ufpe.br>
2016-10-26 19:19:54 -0300

    Keyboard info.

--- src/js/KeyboardControls.js
+++ src/js/KeyboardControls.js
@@ -3,25 +3,36 @@
  * Using Chrome key configuration.
  */
 
+ // 039 >> Right arrow -> Move camera X to right
+ // 037 >> Left arrow -> Move camera X to left
+ // 040 >> Down arrow -> Moves down the camera Y
+ // 038 >> Up arrow -> Moves up the camera Y
+ // 189 >> - key -> Descrease the camera Z
+ // 187 >> + key -> Increase the camera Z
+
+var incrementScale = 1.1;
+var decrementScale = 0.9;
+
 document.onkeydown = function(e) {
+    console.log(e.keyCode);
     switch (e.keyCode) {
         case 37:
-            camera.position.x *= 1.1;
+            camera.position.x *= incrementScale;
             break;
         case 38:
-            camera.position.y *= 0.9;
+            camera.position.y *= decrementScale;
             break;
         case 39:
-            camera.position.x *= 0.9;
+            camera.position.x *= decrementScale;
             break;
         case 40:
-            camera.position.y *= 1.1;
+            camera.position.y *= incrementScale;
             break;
         case 189:
-            camera.position.z *= 1.1;
+            camera.position.z *= incrementScale;
             break;
         case 187:
-            camera.position.z *= 0.9;
+            camera.position.z *= decrementScale;
             break;
         default:
             console.log(">> Warning:\n>> The following number Key was not recognised: "+e.keyCode);
