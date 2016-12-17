cd5f84c24db766050300834e9727cac873519574
Rafael Nunes G. da Silveira <rngs@cin.ufpe.br>
2016-12-17 00:11:49 -0300

    Landing page updated.

--- docs/land-page/js/interface.js
+++ docs/land-page/js/interface.js
@@ -1,12 +1,27 @@
 $(document).ready(function(){
-    /* $('.carousel').carousel({
-           dist:0,
-           shift:0,
-           padding:20,
-
-     });
-    $('.materialboxed').materialbox();*/
-    $('.carousel').carousel();
-      $('.carousel.carousel-slider').carousel({full_width: true});
-
+    $(".button-collapse").sideNav();
 });
+
+$(function() {
+    $('a[href*="#"]:not([href="#"])').click(function() {
+      if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+        if (target.length) {
+          $('html, body').animate({
+            scrollTop: target.offset().top
+          }, 1000);
+          return false;
+        }
+      }
+    });
+});
+
+function openInNewTab(url) {
+    var win = window.open(url, '_blank');
+    if (win) {
+        win.focus();
+    } else {
+        alert('Please allow popups for this website.');
+    }
+}
