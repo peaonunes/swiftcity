8764e65fb4126418baabcef0aa029ca20e04faf5
Rafael Nunes G. da Silveira <rngs@cin.ufpe.br>
2016-12-16 22:53:07 -0300

    Land page.

---
+++ land-page/js/interface.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+    /* $('.carousel').carousel({
+           dist:0,
+           shift:0,
+           padding:20,
+
+     });
+    $('.materialboxed').materialbox();*/
+    $('.carousel').carousel();
+      $('.carousel.carousel-slider').carousel({full_width: true});
+
+});
